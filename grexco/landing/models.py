841d6cf33023915a4b710b1178fccebc8e93591f
Miguel Mesa <mmesas369@gmail.com>
2018-02-01 12:33:27 -0500

    Nomina

--- grexco/landing/models.py
+++ grexco/landing/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
 # Create your models here.
+
+class Contact(models.Model):
+    """
+    Modelo del formulario de contacto
+    """
+    id = models.AutoField(db_column='Id', primary_key=True)
+    nombre = models.CharField(db_column='Nom', max_length=50, null=False)
+    telefono = models.BigIntegerField(db_column='Tel', null=False)
+    empresa = models.CharField(db_column='Emp', max_length=100, null=False)
+    email = models.EmailField(db_column='Email')
+    mensaje = models.CharField(db_column='Msj', max_length=2000, null=False)
+
+    def __str__(self):
+    	return self.id
+
+    def get_absolute_url(self):
+        """
+        Retorna la url para acceder a un mensaje en particular
+        """
+        return reverse('detalle-del-mensaje', args=[str(self.id)])
